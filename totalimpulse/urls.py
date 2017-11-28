4440dd161e16ee387feb2fa3c12db3b45774891c
Pete <pete.friedhoff@gmail.com>
2017-11-28 00:24:39 -0800

    WORKING! got media uploads working, everything working now

--- totalimpulse/urls.py
+++ totalimpulse/urls.py
@@ -1,9 +1,8 @@
+from __future__ import absolute_import, unicode_literals
+
 from django.conf.urls import include, url
-from django.conf.urls.static import static
-from django.views.generic.base import RedirectView
 from django.contrib import admin
 from django.conf import settings
-import os.path
 
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
@@ -30,11 +29,19 @@ urlpatterns = [
     url(r'^plots/', include(plot_urls)),
 ]
 
+# if settings.DEBUG:
+#     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+#     urlpatterns += staticfiles_urlpatterns() # tell gunicorn where static files are in dev mode
+#     urlpatterns += static(settings.MEDIA_URL + 'images/', document_root=os.path.join(settings.MEDIA_ROOT, 'images'))
+#     urlpatterns += [
+#         url(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'blog/images/favicon.ico'))
+#     ]
+
 if settings.DEBUG:
+    from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-    urlpatterns += staticfiles_urlpatterns() # tell gunicorn where static files are in dev mode
-    urlpatterns += static(settings.MEDIA_URL + 'images/', document_root=os.path.join(settings.MEDIA_ROOT, 'images'))
-    urlpatterns += [
-        url(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'blog/images/favicon.ico'))
-    ]
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
