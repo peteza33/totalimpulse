9952df6b81cc44907a488d25dc05ba604e803c7b
Pete <pete.friedhoff@gmail.com>
2017-12-01 07:53:32 -0800

    added Drag plot

--- plots/forms.py
+++ plots/forms.py
@@ -6,3 +6,10 @@ class plot_line_form(forms.Form):
 	y_1 = forms.IntegerField(min_value = 0, max_value = 100)
 	y_2 = forms.IntegerField(min_value = 0, max_value = 100)
 
+
+class sc_life_form(forms.Form):
+	initial_altitdue = forms.FloatField(min_value = 200., max_value = 600.)
+	mass = forms.FloatField(min_value = 0.0)
+	area = forms.FloatField(min_value = 0.0)
+	Cd = forms.FloatField(min_value = 0.0)
+	total_impulse = forms.FloatField(min_value = 0.0)
--- plots/urls.py
+++ plots/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.plots_home, name = "plots_home"),
 
     url(r'^line/', views.plots_line, name = 'plots_line'),
+
+    url(r'^sc-life-with-drag/', views.sc_life_plot, name = 'plots_sc_drag')
 ]
