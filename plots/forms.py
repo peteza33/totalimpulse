5e57be6c7180056225619a12323a673542efc438
Pete <pete.friedhoff@gmail.com>
2017-12-21 00:47:54 -0800

    working thruster performance model

--- plots/forms.py
+++ plots/forms.py
@@ -9,3 +9,24 @@ class sc_life_form(forms.Form):
 
 class delta_v_form(forms.Form):
 	initial_altitude = forms.FloatField(min_value = 160., label = 'Initial Altitude (km)', initial = '500')
+
+class thruster_performance_form(forms.Form):
+	initial_inlet_pressure = forms.FloatField(min_value = 0, label = 'Beginning of Inlet Pressure Range (bar)', initial = '5.5')
+	final_inlet_pressure = forms.FloatField(min_value = 0, label = 'Eng of Inlet Pressure Range (bar)', initial = '22')
+	k1 = forms.FloatField(min_value = 0, label = 'Mass Flow Rate Coefficient K1 (kg/s)', initial = '3.78e-05')
+	k2 = forms.FloatField(min_value = 0, label = 'Mass Flow Rate Coefficient K2 (kg/s)', initial = '0.82')
+	Tci = forms.FloatField(min_value = 0, label = 'Ideal Combustion Temperature from CEA (K)', initial = '1843')
+	gamma = forms.FloatField(min_value = 0, label = 'Ratio of Specific Heats from CEA (gamma)', initial = '1.228')
+	cstari = forms.FloatField(min_value = 0, label = 'Ideal Characteristic Velocity from CEA (m/s)', initial = '1347.2')
+	cf = forms.FloatField(min_value = 0, label = 'Coefficient of Thrust', initial = '1.8382')
+	MW = forms.FloatField(min_value = 0, label = 'Molecular Weight (g/mol', initial = '19.718')
+	Cp = forms.FloatField(min_value = 0, label = 'Specific Heat at Constant Pressure from CEA (J/kg-K)', initial = '2.2741e3')
+	dc = forms.FloatField(min_value = 0, label = 'Chamber Diameter (m)', initial = '0.0127')
+	lc = forms.FloatField(min_value = 0, label = 'Chamber Length (m)', initial = '0.0635')
+	h = forms.FloatField(min_value = 0, label = 'Heat Transfer Coefficient (W/m^2-K)', initial = '500')
+	wall_mass = forms.FloatField(min_value = 0, label = 'Chamber Wall Mass (kg)', initial = '0.01')
+	Cw = forms.FloatField(min_value = 0, label = 'Chamber Wall Specific Heat (J/kg-K)', initial = '137')
+	eta_c = forms.FloatField(min_value = 0, label = 'Combustion Efficieny (Eta)', initial = '0.94')
+	maneuver_press = forms.FloatField(min_value = 0, label = 'Maneuver Pressure to Simulate (bar)', initial = '18.5')
+	maneuver_time = forms.IntegerField(min_value = 0, label = 'Maneuver Time to Simulate (sec)', initial = '10')
+	duty_cycle = forms.FloatField(min_value = 0, label = 'Maneuver Duty Cycle to Simulate (0.0 to 1.0)', initial = '1.0')
--- plots/views.py
+++ plots/views.py
@@ -4,7 +4,7 @@ from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.models import LinearAxis, Range1d
 
-from .forms import sc_life_form, delta_v_form
+from .forms import sc_life_form, delta_v_form, thruster_performance_form
 
 
 def sc_life(request):
@@ -76,25 +76,8 @@ def sc_life(request):
 			    
 			# DV for drag after dropping altitude to mission orbit
 			remaining_DV = DV_capability - DV_alt_drop
-			    
-			"""
-			Estimate drag force
-			"""
-			# # From Orbion atmospher tool for 0.4 solar cycle (range is for alts = np.arange(150, 510, 10))
-			# rho = np.array([1.57503761e-09,   8.71650114e-10,   5.10685020e-10,
-			#          3.11980283e-10,   1.96870776e-10,   1.27554367e-10,
-			#          8.45030934e-11,   5.70666788e-11,   3.91884398e-11,
-			#          2.73077516e-11,   1.92733000e-11,   1.37541417e-11,
-			#          9.91009690e-12,   7.19947796e-12,   5.26742035e-12,
-			#          3.87732881e-12,   2.86913243e-12,   2.13279450e-12,
-			#          1.59179919e-12,   1.19229314e-12,   8.95975607e-13,
-			#          6.75359621e-13,   5.10561785e-13,   3.87100189e-13,
-			#          2.94364743e-13,   2.24542614e-13,   1.71855723e-13,
-			#          1.32014841e-13,   1.01825680e-13,   7.89027203e-14,
-			#          6.14600820e-14,   4.81574247e-14,   3.79887363e-14,
-			#          3.01948922e-14,   2.42039447e-14,   1.95838940e-14])
 
-			# From Orbion atmospher tool for 0.4 solar cycle (range is for alts = np.arange(150, 510, 5))
+			# From NRL drag tool for 0.4 solar cycle (range is for alts = np.arange(150, 510, 5))
 			rho = np.array([1.57503761e-09,   1.16186584e-09,   8.71650114e-10,
 					         6.63182738e-10,   5.10685020e-10,   3.97346939e-10,
 					         3.11980283e-10,   2.46935103e-10,   1.96870776e-10,
@@ -127,25 +110,36 @@ def sc_life(request):
 			life = remaining_DV / (F / m) * 3.17098e-8 # years
 
 			# Create plot
-			plot = figure(plot_width = 1000, plot_height = 800)
+			plot = figure(
+				plot_width = 1000, 
+				plot_height = 800, 
+				y_axis_label = 'Time (years)',
+				x_axis_label = 'Altitude (km)',
+				title = 'Spacecraft Drag Compensation Life after Dropping Altitude from %.0f km' % alt_initial,
+				toolbar_location = "above",
+				y_range = Range1d(0, 30),
+			)
 
 			# Add data
 			plot.circle(alt_final_range, life, color = 'blue')
 			plot.line(alt_final_range, life, legend = 'Lifetime', color = 'blue')
 
-			plot.extra_y_ranges = {"drag": Range1d(start = 0, end = np.amax(F) * 1e3)}
-			plot.add_layout(LinearAxis(y_range_name = "drag", axis_label = "Drag (mN)"), 'left')
-
 			# secondary axis data
+			plot.extra_y_ranges = {"drag": Range1d(start = 0, end = 15)}
+			plot.add_layout(LinearAxis(y_range_name = "drag", axis_label = "Drag Force (mN)"), 'right')
+
 			plot.circle(alt_final_range, F * 1e3, y_range_name = "drag", color = 'red')
 			plot.line(alt_final_range, F * 1e3, y_range_name = "drag", legend = 'Drag', color = 'red')
 
 			# Formatting
-			plot.title.text = 'SpaceCraft Life After Dropping Altitude from %.0f km' % alt_initial
 			plot.title.align = "center"
-			plot.xaxis.axis_label = "Altitude (km)"
-			plot.yaxis.axis_label = "Time (years)"
 			plot.toolbar.logo = None
+			plot.legend.location = "top_left"
+			plot.xaxis.axis_label_text_font_style = "normal"
+			plot.yaxis.axis_label_text_font_style = "normal"
+			plot.xaxis.axis_label_text_font_size = '12pt'
+			plot.yaxis.axis_label_text_font_size = '12pt'
+			plot.title.text_font_size = "12pt"
 			
 			# Embed plot elements
 			script, div = components(plot)
@@ -271,24 +265,33 @@ def delta_v(request):
 			combined_alt_plane_change_SSO_DV = combined_alt_plane_change_SSO_np_func(alt_initial, alt_final_range)
 
 			# Create plot
-			plot = figure(plot_width = 1000, plot_height = 800)
+			plot = figure(
+				plot_width = 1000, 
+				plot_height = 800, 
+				y_axis_label = 'Delta-V (m/s)',
+				x_axis_label = 'Altitude (km)',
+				title = 'Delta-V to Drop Altitude from %.0f km & Maintain SSO' % alt_initial,
+				toolbar_location = "right",
+			)
 
 			# Add data
-			plot.line(alt_final_range, hohmann_transfer_DV, legend = 'Hohmann Transfer', color = 'blue')
-			plot.line(alt_final_range, plane_change_to_maintain_SSO_DV, legend = 'Plane Change to Maintain SSO', color = 'green')
-			plot.line(alt_final_range, total_DV, color = 'red', legend = 'Hohmann + Plane Change (inefficient)')
+			plot.line(alt_final_range, hohmann_transfer_DV, legend = 'Hohmann Transfer', color = 'blue', line_dash = 'dashdot')
+			plot.line(alt_final_range, plane_change_to_maintain_SSO_DV, legend = 'Plane Change to Maintain SSO', color = 'green', line_dash = 'dotdash')
+			plot.line(alt_final_range, total_DV, color = 'red', legend = 'Hohmann + Plane Change (inefficient)', line_dash = 'dashed')
 			plot.line(alt_final_range, combined_alt_plane_change_SSO_DV, color = 'orange', legend = 'Combining Altitude & Plane Change (most efficient)')
 
 			# reverse the x-axis
 			plot.x_range = Range1d(alt_initial, alt_final_range[0] - 10)
 
 			# Formatting
-			plot.title.text = 'Delta-V to Drop Altitude from %.0f km & Maintain SSO' % alt_initial
 			plot.title.align = "center"
-			plot.xaxis.axis_label = "Altitude (km)"
-			plot.yaxis.axis_label = "Delta-V (m/s)"
 			plot.toolbar.logo = None
 			plot.legend.location = "top_left"
+			plot.xaxis.axis_label_text_font_style = "normal"
+			plot.yaxis.axis_label_text_font_style = "normal"
+			plot.xaxis.axis_label_text_font_size = '12pt'
+			plot.yaxis.axis_label_text_font_size = '12pt'
+			plot.title.text_font_size = "12pt"
 			
 			# Embed plot elements
 			script, div = components(plot)
@@ -302,4 +305,211 @@ def delta_v(request):
 
 
 def thruster_performance(request):
-	return render(request, 'thruster_performance.html', {})
+	if request.method == 'POST':
+		form = thruster_performance_form(request.POST)
+		if form.is_valid():
+			data = form.cleaned_data
+
+			import numpy as np
+			from scipy.integrate import ode
+			from scipy.optimize import brentq
+			import math
+
+			# Inlet pressure range
+			p_inlet = np.linspace(data['initial_inlet_pressure'], data['final_inlet_pressure'], 50)
+
+			# Model mass flow rate
+			k1 = data['k1'] 
+			k2 = data['k2']
+			def mdot_f(press):
+				"""
+				Output: kg/s
+				"""
+				return k1 * press ** k2
+
+			"""
+			IDEAL reults from RPA/CEA at 15 bar chamber pressure
+			"""
+			Ru = 8314 # J/kg-K
+			Tci = data['Tci'] # K
+			gamma = data['gamma']
+			cstari = data['cstari'] # m/s
+			cf = data['cf']
+			Mw = data['MW'] # g/mol
+			Cp = data['Cp'] # J/kg-K - at 1843 K
+
+			# characteristic velocity (m/s)
+			def cstar_f(Tc, gamma, Mw):
+				return np.sqrt(Ru * Tc / (gamma * Mw * (2. / (gamma + 1.))**((gamma + 1.) / (gamma - 1.))))
+
+			"""
+			Model to estimate dynamic chamber energy balance. Makes estimates of the radiative area, emissivity and chamber thermal mass
+			"""
+			# Stefan-Boltzmann constant
+			sigma = 5.67e-8 # W / m^2-K^4
+			# gravity
+			g0 = 9.806 # m / s^2
+			# chamber diameter
+			dc = data['dc'] # m (convert inches to m)
+			# chamber length
+			lc = data['lc'] # m (convert inches to m)
+			# Wall area
+			Ac = math.pi * dc * lc
+			# heat transfer coefficient
+			h = data['h'] # W/m^2-K
+			# wall mass
+			mw = data['wall_mass'] # kg
+			# wall specific heat
+			Cw = data['Cw'] # J/kg-K
+			# space temp
+			Tinf = 3. # K
+			# ambient temp
+			T0 = 273. # K
+
+			# emissivity coefficient
+			epsilon_f = lambda T: 0.9 - T / 3000.
+
+			# combustion efficiency factor (used to make Isp = 231.5 at steady state with p_inlet = 22 (p_c = 15))
+			# RPA/CEA results of overall estimated efficiency is 0.9417
+			eta_c = data['eta_c']
+
+			# Dynamic chamber energy balance
+			def temp_dyn(mdot, Tw_i = 623., DC = 1., eta_c = 0.9, rate = 1., steps_per_period = 100):
+
+				# enthalpy of reaction
+				del_h_rxn = eta_c * Cp * (Tci - T0)
+
+				# time
+				tv = [0.0]
+
+				# initial wall temp
+				Tw = [Tw_i]
+
+				# mass flow
+				mdot_v = [mdot]
+
+				# final time
+				tf = rate * len(DC)
+
+				def Tc_f(Tw, mdot_v):
+					"""
+					Combustion Temperature
+					"""
+					return (mdot_v*del_h_rxn + h*Ac*Tw + mdot_v*Cp*T0)/(mdot_v*Cp + h*Ac)
+
+				def Tw_dot(t, Tw):
+					'''
+					Chamber Wall Temperature
+					'''
+					# Combustion temperature
+					Tc = Tc_f(Tw, on*mdot)
+
+					# Chamber wall temp with time
+					dTw_dt = (h*Ac*(Tc-Tw) - sigma*epsilon_f(Tw)*Ac*(Tw**4 - Tinf**4)) / (mw*Cw)
+					return dTw_dt
+
+				# Pass chamber wall temperature function into ODE solver (runge-kutta)
+				r = ode(Tw_dot).set_integrator('dopri5', atol=1e-3, rtol=1e-3, max_step=0.1, verbosity=1)
+
+				# Set start temperature and start time
+				r.set_initial_value(Tw_i, 0.0)
+
+				# Count
+				i = 0
+
+				# Time step
+				dt = 1. / rate / steps_per_period
+
+				while r.successful() and r.t < tf and i < len(DC):
+					# period's initial time
+					tper_0 = r.t
+
+					# Set thruster ON to include combustion heat flux
+					on = 1.0
+					for j in range(steps_per_period):
+
+						# integrate over time step
+						r.integrate(r.t + dt)
+
+						# record time
+						tv.append(r.t)
+
+						# record wall temperature
+						Tw.append(r.y)
+
+						# record mass flow if engine is ON
+						mdot_v.append(on * mdot)
+
+						# check if thruster is ON for this time step
+						if (r.t - tper_0) >= (DC[i] / rate):
+						    on = 0.0
+					i += 1
+		    
+				# combustion temperature
+				Tc = Tc_f(np.array(Tw), np.array(mdot_v))
+
+				# return time, wall temp, and mass flow
+				return np.array(tv), np.array(Tw), Tc, np.array(mdot_v)
+
+			"""
+			Run the dynamic chamber temp energy balance 
+			"""
+			# Inlet pressure bar(a)
+			Pf = data['maneuver_press'] # bar
+
+			# Duration (sec)
+			maneuver_time = data['maneuver_time'] # s
+
+			# Duty Cycle (in percent)
+			duty_cycle = data['duty_cycle']
+
+			# Run combustion chamber energy balance
+			tv, Tw, Tc, mdot_v = temp_dyn(mdot_f(Pf), eta_c = eta_c, DC = (np.ones([maneuver_time]) * duty_cycle))
+
+			# characteristic velocity (m/s)
+			cstar_t = cstar_f(Tc, gamma, Mw)
+
+			# exhaust velocity (m/s)
+			c_t = cstar_t * cf
+
+			"""
+			Plot
+			"""
+			plot = figure(
+				plot_width = 1000, 
+				plot_height = 800, 
+				y_axis_label = 'Temperature (K)',
+				x_axis_label = 'Time (sec)',
+				title = 'Transient Model of Combustion Temperature and Performance',
+				toolbar_location = "below",
+				y_range = Range1d(600, 1600),
+			)
+
+			# primary axis
+			plot.line(tv, Tc, legend = 'Gas Temp', color = 'red')
+			plot.line(tv, Tw, legend = 'Wall Temp', color = 'blue', line_dash = 'dashdot')
+
+			# secondary axis
+			plot.extra_y_ranges = {"Isp": Range1d(start = 150, end = 250)}
+			plot.add_layout(LinearAxis(y_range_name = "Isp", axis_label = "Specific Impulse (sec)"), 'right')
+
+			plot.line(tv, c_t / g0, y_range_name = "Isp", legend = 'Isp', color = 'green', line_dash = 'dashed')
+
+			# Formatting
+			plot.title.align = "center"
+			plot.toolbar.logo = None
+			plot.legend.location = "bottom_right"
+			plot.xaxis.axis_label_text_font_style = "normal"
+			plot.yaxis.axis_label_text_font_style = "normal"
+			plot.xaxis.axis_label_text_font_size = '12pt'
+			plot.yaxis.axis_label_text_font_size = '12pt'
+			plot.title.text_font_size = "12pt"
+			
+			# Embed plot elements
+			script, div = components(plot)
+
+			return render(request, "thruster_performance.html", {"bokeh_plot_script": script, 'bokeh_plot_div': div})
+	else:
+		form = thruster_performance_form()
+
+	return render(request, "thruster_performance.html", {'form': form})
